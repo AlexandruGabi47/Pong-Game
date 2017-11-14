01de621a0aeb28675369b8b14e20465f915faf0b
AlexandruGabi47 <32970433+AlexandruGabi47@users.noreply.github.com>
2017-11-14 20:31:45 +0200

    Add files via upload

---
+++ Pong/Pong.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Pong
+{
+	partial class Pong
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.Player1 = new System.Windows.Forms.Label();
+			this.Player2 = new System.Windows.Forms.Label();
+			this.scoreLabel2 = new System.Windows.Forms.Label();
+			this.scoreLabel1 = new System.Windows.Forms.Label();
+			this.CountdownLabel = new System.Windows.Forms.Label();
+			this.pongBall = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pongBall)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Interval = 15;
+			this.timer.Tick += new System.EventHandler(this.TimeTickHandler);
+			// 
+			// Player1
+			// 
+			this.Player1.BackColor = System.Drawing.Color.Gray;
+			this.Player1.Location = new System.Drawing.Point(12, 190);
+			this.Player1.Name = "Player1";
+			this.Player1.Size = new System.Drawing.Size(10, 80);
+			this.Player1.TabIndex = 0;
+			// 
+			// Player2
+			// 
+			this.Player2.BackColor = System.Drawing.Color.Gray;
+			this.Player2.Location = new System.Drawing.Point(867, 190);
+			this.Player2.Name = "Player2";
+			this.Player2.Size = new System.Drawing.Size(10, 80);
+			this.Player2.TabIndex = 1;
+			// 
+			// scoreLabel2
+			// 
+			this.scoreLabel2.AutoSize = true;
+			this.scoreLabel2.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.scoreLabel2.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.scoreLabel2.Location = new System.Drawing.Point(826, 9);
+			this.scoreLabel2.Name = "scoreLabel2";
+			this.scoreLabel2.Size = new System.Drawing.Size(34, 32);
+			this.scoreLabel2.TabIndex = 4;
+			this.scoreLabel2.Text = "0";
+			// 
+			// scoreLabel1
+			// 
+			this.scoreLabel1.AutoSize = true;
+			this.scoreLabel1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.scoreLabel1.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.scoreLabel1.Location = new System.Drawing.Point(29, 9);
+			this.scoreLabel1.Name = "scoreLabel1";
+			this.scoreLabel1.Size = new System.Drawing.Size(29, 40);
+			this.scoreLabel1.TabIndex = 3;
+			this.scoreLabel1.Text = "0";
+			this.scoreLabel1.UseCompatibleTextRendering = true;
+			// 
+			// CountdownLabel
+			// 
+			this.CountdownLabel.AutoSize = true;
+			this.CountdownLabel.Font = new System.Drawing.Font("Verdana", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CountdownLabel.ForeColor = System.Drawing.Color.Black;
+			this.CountdownLabel.Location = new System.Drawing.Point(386, 34);
+			this.CountdownLabel.Name = "CountdownLabel";
+			this.CountdownLabel.Size = new System.Drawing.Size(58, 57);
+			this.CountdownLabel.TabIndex = 6;
+			this.CountdownLabel.Text = "0";
+			this.CountdownLabel.Visible = false;
+			// 
+			// pongBall
+			// 
+			this.pongBall.BackColor = System.Drawing.Color.SlateGray;
+			this.pongBall.Location = new System.Drawing.Point(411, 215);
+			this.pongBall.Name = "pongBall";
+			this.pongBall.Size = new System.Drawing.Size(10, 10);
+			this.pongBall.TabIndex = 0;
+			this.pongBall.TabStop = false;
+			// 
+			// Pong
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Lavender;
+			this.ClientSize = new System.Drawing.Size(889, 472);
+			this.Controls.Add(this.pongBall);
+			this.Controls.Add(this.CountdownLabel);
+			this.Controls.Add(this.scoreLabel2);
+			this.Controls.Add(this.scoreLabel1);
+			this.Controls.Add(this.Player2);
+			this.Controls.Add(this.Player1);
+			this.Name = "Pong";
+			this.Text = "Form1";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyDown);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyUp);
+			((System.ComponentModel.ISupportInitialize)(this.pongBall)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.Label Player1;
+		private System.Windows.Forms.Label Player2;
+		private System.Windows.Forms.Label scoreLabel2;
+		private System.Windows.Forms.Label scoreLabel1;
+		private System.Windows.Forms.Label CountdownLabel;
+		private System.Windows.Forms.PictureBox pongBall;
+	}
+}
+
---
+++ Pong/Pong.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+//TODOS: angle, increasing speed
+
+namespace Pong
+{
+	public partial class Pong : Form
+	{
+		Random rnd = new Random();
+		Timer countdown = new Timer { Interval = 1000 };
+		int countdownTick = 2;
+
+		bool up1IsPressed = false;
+		bool down1IsPressed = false;
+		bool up2IsPressed = false;
+		bool down2IsPressed = false;
+
+		bool Xplus = false;//transform this into angle
+		bool Yplus = false;
+
+		int player1Score = 0;
+		int player2Score = 0;
+
+		int platformSpeed = 3;
+		int ballSpeed = 3;
+		bool moveBall = true;
+
+		public Pong()
+		{
+			InitializeComponent();
+			countdown.Tick += CountdownTickHandler;
+			
+			if (rnd.Next(0, 2) == 1) Xplus = false;
+			else Xplus = true;
+
+			//Fixes the screen flickering
+			int style = NativeWinAPI.GetWindowLong(this.Handle, NativeWinAPI.GWL_EXSTYLE);
+			style |= NativeWinAPI.WS_EX_COMPOSITED;
+			NativeWinAPI.SetWindowLong(this.Handle, NativeWinAPI.GWL_EXSTYLE, style);
+		}
+
+		private void TimeTickHandler(object sender, EventArgs e)
+		{
+			//Updating the score labels
+			scoreLabel1.Text = $"{player1Score}";
+			scoreLabel2.Text = $"{player2Score}";
+			//Handling the controls
+			if (up1IsPressed)
+				if (!(Player1.Top <= 0))
+					Player1.Top -= platformSpeed;
+			if (down1IsPressed)
+				if (!(Player1.Bottom >= ClientSize.Height))
+					Player1.Top += platformSpeed;
+
+            if (up2IsPressed)
+				if (!(Player2.Top <= 0))
+					Player2.Top -= platformSpeed;
+			if (down2IsPressed)
+				if (!(Player2.Bottom >= ClientSize.Height))
+					Player2.Top += platformSpeed;
+			//Checking if it hit a platform
+			if (pongBall.Bounds.IntersectsWith(Player1.Bounds) && !(pongBall.Location.X < Player1.Location.X + Player1.Width - 3))
+				Xplus = !Xplus;
+			if (pongBall.Bounds.IntersectsWith(Player2.Bounds) && !(pongBall.Location.X > Player2.Location.X + Player2.Width + 3))
+				Xplus = !Xplus;
+			//Checking if it's out of bounds
+			if (pongBall.Bottom >= ClientSize.Height || pongBall.Top <= 0)
+				Yplus = !Yplus;
+
+			if (pongBall.Left + pongBall.Width >= ClientSize.Width)
+				SomeoneScored(ref player1Score);
+
+			if (pongBall.Left <= 0)
+				SomeoneScored(ref player2Score);
+			//Moving the ball
+			if (moveBall)
+			{
+				if (Xplus) pongBall.Left += ballSpeed;
+				else pongBall.Left -= ballSpeed;
+
+				if (Yplus) pongBall.Top += ballSpeed;
+				else pongBall.Top -= ballSpeed;
+			}
+		}
+
+		public void SomeoneScored(ref int playerScore)
+		{
+			playerScore++;
+			GameCountdown();
+		}
+
+		public void GameCountdown()
+		{
+			//Changing direction
+			Xplus = !Xplus;
+			//Moving the ball in the middle of the screen
+			moveBall = false;
+			pongBall.Left = ClientSize.Width / 2;
+			pongBall.Top = ClientSize.Height / 2;
+			//Starting the countdown
+			CountdownLabel.Visible = true;
+			CountdownLabel.Text = $"{countdownTick}";
+			CountdownLabel.Refresh();
+			countdown.Start();
+		}
+
+		private void CountdownTickHandler(object sender, EventArgs e)
+		{
+			if (countdownTick == 0)
+			{
+				countdown.Stop();
+				CountdownLabel.Visible = false;
+				countdownTick = 2;
+				moveBall = true;
+			}
+			countdownTick--;
+			CountdownLabel.Text = $"{countdownTick}";
+			CountdownLabel.Refresh();
+		}
+
+		private void Pong_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.W)
+				up1IsPressed = true;
+			if (e.KeyCode == Keys.S)
+				down1IsPressed = true;
+			if (e.KeyCode == Keys.Up)
+				up2IsPressed = true;
+			if (e.KeyCode == Keys.Down)
+				down2IsPressed = true;
+		}
+
+		private void Pong_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.W)
+				up1IsPressed = false;
+			if (e.KeyCode == Keys.S)
+				down1IsPressed = false;
+			if (e.KeyCode == Keys.Up)
+				up2IsPressed = false;
+			if (e.KeyCode == Keys.Down)
+				down2IsPressed = false;
+		}
+	}
+
+	//Fixes the screen flickering
+	internal static class NativeWinAPI
+	{
+		internal static readonly int GWL_EXSTYLE = -20;
+		internal static readonly int WS_EX_COMPOSITED = 0x02000000;
+
+		[DllImport("user32")]
+		internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+		[DllImport("user32")]
+		internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+	}
+}
